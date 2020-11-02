982b02b94df45987e5ec412d73c518115ccf5037
Husky <space.dog.odyssey@gmail.com>
2020-11-02 16:38:00 +0200

    Start

---
+++ src/view/MainWidnow.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import net.thegreshams.firebase4j.service.Firebase;
+
+public class MainWidnow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
